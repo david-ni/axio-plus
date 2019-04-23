c93b06dd8a6da058e1e4d7da3f3ac96d05091a4d
wangguanjie <wangguanjie@gengee.cn>
2019-04-23 11:58:25 +0800

    [Fix] 测试用例非法，但jest却通过了

--- __tests__/client.spec.ts
+++ __tests__/client.spec.ts
@@ -2,6 +2,7 @@
  * @file 测试用例
  * @author david wang 
  */
+import process from "process";
 import { AxioPlus } from "../index";
 import { HttpErrorCodeType } from "../src/enum";
 describe('http client',()=>{
@@ -19,12 +20,15 @@ describe('http client',()=>{
                 console.log(response);
             })
     });
-    test('listen',()=>{
+    test('listen notFound',(done)=>{
         const httpClient = AxioPlus.createClient({baseUrl: "http://192.168.30.20/"});
         httpClient.listen((topic)=>{
+            // https://stackoverflow.com/questions/50930249/failing-expect-inside-subscribe-does-not-mark-test-as-invalid
+            // fix bug https://github.com/superTerrorist/axio-plus/issues/1
             expect(topic).toBe(HttpErrorCodeType.NotFound);
+            done();
         });
         (httpClient as any).get('/notFound',{})
-            .subscribe(({result,response})=>{})
-    })
+            .subscribe(({result,response})=>{});
+    });
 })
